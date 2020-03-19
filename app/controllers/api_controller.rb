9643f10178ca7783edd1c0db4e772334c67687e8
Michaela Schößwendter <mschoesswendter.mmt-b2018@fh-salzburg.ac.at>
2020-03-19 16:24:09 +0100

    test & controller auskommentiert

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,5 @@
-namespace :api do
-    namespace :v1 do
-      resources :users, only: [:index, :create, :show, :update, :destroy]
-    end
-  end
+# namespace :api do
+#     namespace :v1 do
+#       resources :users, only: [:index, :create, :show, :update, :destroy]
+#     end
+#   end
