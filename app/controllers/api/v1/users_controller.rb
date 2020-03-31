9241c59e85d24539615b70aff09b7068557012f4
Michaela Schößwendter <mschoesswendter.mmt-b2018@fh-salzburg.ac.at>
2020-03-31 17:39:12 +0200

    tryout create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,18 +14,18 @@ class Api::V1::UsersController < Api::V1::BaseController
     end
 
     def create
-        @user = User.new(register_user_params)
+        @user = User.new(user_params)
 
         respond_to do |format|
             if @user.save
                 render json: UserSerializer.new(@user).serialized_json, status: 201
             else
-                format.json { render json: @user.errors, status: :unprocessable_entity }
+                render json: { errors: ErrorSerializer.new(@user).serialized_json }, status: 422
             end
         end
     end
 
-    def register_user_params
+    def user_params
         p = params.require(:data).permit(:type, attributes: %i[name email password])
         
         if p[:type] == 'user'
