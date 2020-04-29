896f694687ba0f4c5d0cf4bde3ff2b5e218d4af0
Michaela Schößwendter <mschoesswendter.mmt-b2018@fh-salzburg.ac.at>
2020-04-29 18:09:00 +0200

    add money transaction

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*', 'localhost:3000', '127.0.0.1:3000', 'https://vigilant-mcnulty-c96704.netlify.com/'
       resource '/api/v1/*',
-        methods: %i(get post put patch delete options head)
+        headers: :any,methods: %i(get post put patch delete options head)
     end
   end
